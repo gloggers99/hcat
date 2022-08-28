204fb4d05523cd178e2acaf1560a18d25566fc2f
gloggers <89711404+gloggers99@users.noreply.github.com>
2022-08-28 12:46:30 -0700

    Update hcat.hs
    amazing

--- hcat.hs
+++ hcat.hs
@@ -1,20 +1,9 @@
 import System.Environment
-import System.IO
 import System.Directory
 
-import Control.Monad
-
--- check which files inputted are actually files
 getFiles :: [String] -> IO [String]
-getFiles = filterM doesFileExist
+getFiles = mapM (\x -> case x of
+                    doesFileExist -> readFile x)
 
--- print a file into stdout
-printFile :: String -> IO ()
-printFile file = do
-    content <- readFile file
-    putStr content
-
--- compose everything together
 main :: IO ()
-main = getArgs >>= getFiles >>= mapM_ printFile
-
+main = getArgs >>= getFiles >>= putStr . unlines
